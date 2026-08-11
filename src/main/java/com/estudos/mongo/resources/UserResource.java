3a1d711965cccb87f9fb4aa7bb5defcbb68f3bd8
Danielle Nunes <daniellesnunes@gmail.com>
2026-08-11 18:28:15 -0300

    feat: implementa GetById e tratamento de exceção

--- src/main/java/com/estudos/mongo/resources/UserResource.java
+++ src/main/java/com/estudos/mongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.estudos.mongo.resources;
 
 import com.estudos.mongo.domain.User;
+import com.estudos.mongo.dto.UserDTO;
 import com.estudos.mongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -17,9 +20,16 @@ public class UserResource {
     @Autowired
     private UserService service;
 
-    @RequestMapping(method= RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(UserDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 }
